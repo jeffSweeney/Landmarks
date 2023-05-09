21ac5798214d413814a452c80a817c0d44eee50d
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2023-05-08 20:06:00 -0400

    Initialize template app

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Jeffrey Sweeney on 5/8/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
