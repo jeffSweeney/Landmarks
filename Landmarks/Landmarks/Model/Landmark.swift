b377df7b9e5107092a98c1d61603a069858ee889
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2023-05-09 21:24:30 -0400

    Restructure files - extend Landmark model - provide model for loading data from the json

---
+++ Landmarks/Landmarks/Model/Landmark.swift
@@ -0,0 +1,39 @@
+//
+//  Landmark.swift
+//  Landmarks
+//
+//  Created by Jeffrey Sweeney on 5/9/23.
+//
+
+import CoreLocation
+import Foundation
+import SwiftUI
+
+struct Landmark: Codable, Hashable {
+    // MARK: - Decoded Properties
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    var imageName: String
+    private var coordinates: Coordinates
+    
+    // MARK: - Computed Properties
+    var image: Image {
+        Image(imageName)
+    }
+    
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude
+        )
+    }
+    
+    // MARK: - Nested Types
+    struct Coordinates: Codable, Hashable {
+        var latitude: Double
+        var longitude: Double
+    }
+}
---
+++ Landmarks/Landmarks/Model/ModelData.swift
@@ -0,0 +1,31 @@
+//
+//  ModelData.swift
+//  Landmarks
+//
+//  Created by Jeffrey Sweeney on 5/9/23.
+//
+
+import Foundation
+
+var landmarks: [Landmark] = load("landmarkData.json")
+
+func load<T: Decodable>(_ filename: String) -> T {
+    let data: Data
+    
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil) else {
+        fatalError("Couldn't locate \(filename) in main bundle.")
+    }
+    
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("Couldn't load data from \(filename): \(error)")
+    }
+    
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    } catch {
+        fatalError("Couldn't parse \(filename) as \(T.self): \(error)")
+    }
+}
