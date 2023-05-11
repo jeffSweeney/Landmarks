58ad123c3586fe0bb85a8dd666fe483e24bf2c39
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2023-05-10 20:58:04 -0400

    Add image property for the circle image for reuse

--- Landmarks/Landmarks/Views/CircleImage.swift
+++ Landmarks/Landmarks/Views/CircleImage.swift
@@ -8,8 +8,10 @@
 import SwiftUI
 
 struct CircleImage: View {
+    var image: Image
+    
     var body: some View {
-        Image("turtlerock")
+        image
             .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
             .overlay {
                 Circle().stroke(.white, lineWidth: 4)
@@ -20,6 +22,9 @@ struct CircleImage: View {
 
 struct CircleImage_Previews: PreviewProvider {
     static var previews: some View {
-        CircleImage()
+        Group {
+            CircleImage(image: Image("turtlerock"))
+            CircleImage(image: Image("icybay"))
+        }
     }
 }
